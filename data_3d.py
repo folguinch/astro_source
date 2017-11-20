912a54272a43aa96db9ba65f1ba56b4e0ee745e7
Fernando Olguin <f.olguin.ch@gmail.com>
2017-11-20 13:59:36 +0800

    Change default frame to fk5

--- data_3d.py
+++ data_3d.py
@@ -1,6 +1,8 @@
 from spectral_cube import SpectralCube
 from astropy.io import fits
 from astropy.wcs import WCS
+from astropy.coordinates import SkyCoord
+import astropy.units as u
 from myutils.logger import get_logger
 
 from .data import Data
@@ -38,3 +40,25 @@ class Data3D(Data):
     def save(self, filename=None, overwrite=True):
         """Save the data cube"""
         self.data.write(filename or self.address, overwrite=overwrite)
+
+    @property
+    def wcs(self):
+        """Return the WCS with the position data."""
+        return self.data.wcs.sub(['longitude', 'latitude'])
+
+    def get_coord(self, xpix, ypix, frame='fk5'):
+        """Return the (ra, dec) coordinates of the input pixel location.
+
+        Parameters:
+            xpix (float): x-position of the coordinate.
+            ypix (float): y-position of the coordinate.
+            frame (str, default=icrs): sky frame projection.
+
+        Returns:
+            coord (astropy.SkyCoord): sky coordinate of the input location.
+
+        Note:
+            xpix and ypix are zero based.
+        """
+        ra, dec = self.wcs.all_pix2world([[xpix, ypix]], 0)[0]
+        return SkyCoord(ra=ra*u.degree, dec=dec*u.degree, frame=frame)
