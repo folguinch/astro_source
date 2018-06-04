03d64c40fcfeadab52f4dcd63bd05556edb38c23
Fernando Olguin <f.olguin.ch@gmail.com>
2018-06-04 16:33:36 +0800

    Add Cube loading function

--- argparse_actions.py
+++ argparse_actions.py
@@ -1,6 +1,7 @@
 import os, argparse
 
 from .image import Image
+from .data_cube import Cube
 
 class LoadFITS(argparse.Action):
     """Action class for loading fits files with atroSource Image"""
@@ -24,4 +25,25 @@ class LoadFITS(argparse.Action):
                         exit()
                     images += [Image(val)]
         setattr(namespace, self.dest, images)
+
+class LoadCube(argparse.Action):
+    """Action class for loading fits files with atroSource Image"""
+
+    def __init__(self, option_strings, dest, nargs=None, **kwargs):
+
+        if nargs is not None and nargs>1:
+            raise ValueError("only nargs=1 allowed")
+        super(LoadCube, self).__init__(option_strings, dest, nargs=nargs, **kwargs)
+
+    def __call__(self, parser, namespace, values, option_string=None):
+
+        if len(values)==1 and not hasattr(values, 'lower'):
+            fname = values[0]
+        else:
+            fname = values
+
+        fname = os.path.expanduser(fname)
+        cube = Cube(fname)
+
+        setattr(namespace, self.dest, cube)
 
